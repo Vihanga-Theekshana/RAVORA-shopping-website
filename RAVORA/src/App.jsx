7cb8267d01999b85f0d64d93085799f401b54a34
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2026-02-03 18:16:53 +0530

    implement womenscloth page

--- RAVORA/src/App.jsx
+++ RAVORA/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import Menclothing from "./pages/Menclothingpage";
+import Womenclothing from "./pages/Womenclothing";
 
 function App() {
   return (
     <>
       {/* <Homepage /> */}
-      <Menclothing />
+      {/* <Menclothing /> */}
+      <Womenclothing />
     </>
   );
 }
