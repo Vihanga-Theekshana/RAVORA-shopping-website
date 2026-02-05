c19f768a992b863106a49412e1560a194e240d61
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2026-02-05 21:20:12 +0530

    implement login and signup pages

--- RAVORA/src/App.jsx
+++ RAVORA/src/App.jsx
@@ -5,20 +5,23 @@ import Navbar from "./components/common/Navbar/Navbar";
 import Homepage from "./pages/Homepage";
 import Menclothing from "./pages/Menclothingpage";
 import Womenclothing from "./pages/Womenclothing";
-import Login from "./pages/Login";
-
+import Login from "./pages/Login&Registration/Login";
+import Signup from "./pages/Login&Registration/Signup";
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Routes>
+
+      <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Men" element={<Menclothing />} />
         <Route path="/Women" element={<Womenclothing />} />
-      </Routes> */}
+      </Routes>
       {/* <Homepage /> */}
       {/* <Menclothing /> */}
-      <Login />
+      {/* <Login /> */}
+      {/* <Signup />
+       */}
       <Footer />
     </>
   );
