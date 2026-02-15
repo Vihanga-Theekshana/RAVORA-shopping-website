415ca9ca382820dbd7d19e9e88cde9020595c5f6
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2026-02-15 12:07:41 +0530

    implement register frontend

--- RAVORA/src/pages/Login&Registration/Signup.jsx
+++ RAVORA/src/pages/Login&Registration/Signup.jsx
@@ -1,16 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function Example() {
+  const [username, setusername] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [message, setmessage] = useState("");
+
+  const handelsubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+      );
+      setmessage(response.data.message);
+
+      setusername("");
+      setemail("");
+      setpassword("");
+    } catch (err) {
+      setmessage(err.response?.data?.message || "Error register user");
+    }
+  };
+
   return (
     <div className="flex justify-center items-center p-5">
-      <form className="bg-white text-gray-500 w-full max-w-[340px] mx-4 md:p-6 p-4 py-8 text-left text-sm rounded-lg shadow-[0px_0px_10px_0px] shadow-black/10">
+      <form
+        className="bg-white text-gray-500 w-full max-w-[340px] mx-4 md:p-6 p-4 py-8 text-left text-sm rounded-lg shadow-[0px_0px_10px_0px] shadow-black/10"
+        onSubmit={handelsubmit}
+      >
         <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
           Sign Up
         </h2>
 
         <input
-          id="email"
+          id="username"
           className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3"
           type="text"
           placeholder="Username"
+          value={username}
+          onChange={(e) => setusername(e.target.value)}
           required
         />
         <input
@@ -18,19 +52,24 @@ export default function Example() {
           className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3"
           type="email"
           placeholder="Email"
+          value={email}
+          onChange={(e) => setemail(e.target.value)}
           required
         />
         <input
-          id="email"
+          id="password"
           className="w-full border mt-1 bg-indigo-500/5 mb-7 border-gray-500/10 outline-none rounded py-2.5 px-3"
-          type="text"
+          type="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
           required
         />
 
         <button className="w-full mb-3 bg-gray-700 hover:bg-black transition-all active:scale-95 py-2.5 rounded text-white font-medium">
           Create Account
         </button>
+        <p>{message}</p>
 
         <p className="text-center mt-4">
           Already have an account?{" "}
