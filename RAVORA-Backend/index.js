b48f1432987cd696d48f9f2c888c513a5b3ea989
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2026-02-17 11:49:05 +0530

    add stop propergation for login page

--- RAVORA-Backend/index.js
+++ RAVORA-Backend/index.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(cors());
 app.use(session({secret:process.env.JWT_SECRET,resave:false, saveUninitialized:false}));
 app.use(express.json());
+
 app.use("/api/auth",authroute);
-
-
 
 console.log('server listening on port: '+ process.env.PORT);
 app.listen(process.env.PORT);
