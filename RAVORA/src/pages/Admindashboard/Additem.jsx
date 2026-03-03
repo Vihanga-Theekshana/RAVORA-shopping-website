93fe383ce817436d5733f5f9f575bacaad351ba3
Vihanga Theekshana <theekshanavihanga2@gmail.com>
2026-03-03 20:19:44 +0530

    add item live itemcard implement

--- RAVORA/src/pages/Admindashboard/Additem.jsx
+++ RAVORA/src/pages/Admindashboard/Additem.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
+import Itemcard from "../../components/Items/Itemcard";
 const Additem = () => {
   const [name, setname] = useState("");
   const [description, setdescription] = useState("");
@@ -7,9 +8,13 @@ const Additem = () => {
   const [price, setprice] = useState("");
   const [offerprice, setofferprice] = useState("");
   const [images, setimages] = useState([]);
+  const [preview, setpreview] = useState(null);
 
   const handelimage = (e) => {
-    setimages(Array.from(e.target.files));
+    const files = Array.from(e.target.files);
+    setimages(files);
+    const file = files[0];
+    setpreview(URL.createObjectURL(file));
   };
 
   const handelsubmit = async (e) => {
@@ -37,117 +42,139 @@ const Additem = () => {
   };
 
   return (
-    <div className="py-10 flex flex-col justify-between bg-white">
-      <form className="md:p-10 p-4 space-y-5 max-w-lg" onSubmit={handelsubmit}>
-        <div>
-          <p className="text-base font-medium">Product Image</p>
-          <div className="flex flex-wrap items-center gap-3 mt-2">
-            <label>
-              <input
-                accept="image/*"
-                type="file"
-                name="images"
-                multiple
-                hidden
-                onChange={handelimage}
-              />
-              <img
-                className="max-w-24 cursor-pointer"
-                src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/e-commerce/uploadArea.png"
-                alt="uploadArea"
-                width={100}
-                height={100}
-              />
-            </label>
-          </div>
-        </div>
-        <div className="flex flex-col gap-1 max-w-md">
-          <label className="text-base font-medium" htmlFor="product-name">
-            Product Name
-          </label>
-          <input
-            id="product-name"
-            type="text"
-            placeholder="Type here"
-            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
-            value={name}
-            onChange={(e) => setname(e.target.value)}
-            required
-          />
-        </div>
-        <div className="flex flex-col gap-1 max-w-md">
-          <label
-            className="text-base font-medium"
-            htmlFor="product-description"
-          >
-            Product Description
-          </label>
-          <textarea
-            id="product-description"
-            rows={4}
-            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none"
-            value={description}
-            onChange={(e) => setdescription(e.target.value)}
-            placeholder="Type here"
-          ></textarea>
-        </div>
-        <div className="w-full flex flex-col gap-1">
-          <label className="text-base font-medium" htmlFor="category">
-            Category
-          </label>
-          <select
-            id="category"
-            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
-            value={category}
-            onChange={(e) => setcategory(e.target.value)}
-          >
-            <option value="">Select Category</option>
-            {[{ name: "Mensclothing" }, { name: "Womensclothing" }].map(
-              (item, index) => (
-                <option key={index} value={item.name}>
-                  {item.name}
-                </option>
-              ),
-            )}
-          </select>
-        </div>
-        <div className="flex items-center gap-5 flex-wrap">
-          <div className="flex-1 flex flex-col gap-1 w-32">
-            <label className="text-base font-medium" htmlFor="product-price">
-              Product Price
-            </label>
-            <input
-              id="product-price"
-              type="number"
-              placeholder="0"
-              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
-              value={price}
-              onChange={(e) => setprice(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex-1 flex flex-col gap-1 w-32">
-            <label className="text-base font-medium" htmlFor="offer-price">
-              Offer Price
-            </label>
-            <input
-              id="offer-price"
-              type="number"
-              placeholder="0"
-              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
-              value={offerprice}
-              onChange={(e) => setofferprice(e.target.value)}
-              required
-            />
-          </div>
-        </div>
-        <button
-          type="submit"
-          className="px-8 py-2.5 bg-indigo-500 text-white font-medium rounded"
+    <div className="flex w-full">
+      <div className="py-10 flex flex-col  bg-white w-full">
+        <form
+          className="md:p-10 p-4 space-y-5 max-w-lg"
+          onSubmit={handelsubmit}
         >
-          ADD
-        </button>
-      </form>
+          <div>
+            <p className="text-base font-medium">Product Image</p>
+            <div className="flex flex-wrap items-center gap-3 mt-2">
+              <label>
+                <input
+                  accept="image/*"
+                  type="file"
+                  name="images"
+                  multiple
+                  hidden
+                  onChange={handelimage}
+                />
+                <img
+                  className="max-w-24 cursor-pointer"
+                  src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/e-commerce/uploadArea.png"
+                  alt="uploadArea"
+                  width={100}
+                  height={100}
+                />
+              </label>
+            </div>
+          </div>
+          <div className="flex flex-col gap-1 max-w-md">
+            <label className="text-base font-medium" htmlFor="product-name">
+              Product Name
+            </label>
+            <input
+              id="product-name"
+              type="text"
+              placeholder="Type here"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-1 max-w-md">
+            <label
+              className="text-base font-medium"
+              htmlFor="product-description"
+            >
+              Product Description
+            </label>
+            <textarea
+              id="product-description"
+              rows={4}
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none"
+              value={description}
+              onChange={(e) => setdescription(e.target.value)}
+              placeholder="Type here"
+            ></textarea>
+          </div>
+          <div className="w-full flex flex-col gap-1">
+            <label className="text-base font-medium" htmlFor="category">
+              Category
+            </label>
+            <select
+              id="category"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              value={category}
+              onChange={(e) => setcategory(e.target.value)}
+            >
+              <option value="">Select Category</option>
+              {[{ name: "Mensclothing" }, { name: "Womensclothing" }].map(
+                (item, index) => (
+                  <option key={index} value={item.name}>
+                    {item.name}
+                  </option>
+                ),
+              )}
+            </select>
+          </div>
+          <div className="flex items-center gap-5 flex-wrap">
+            <div className="flex-1 flex flex-col gap-1 w-32">
+              <label className="text-base font-medium" htmlFor="product-price">
+                Product Price
+              </label>
+              <input
+                id="product-price"
+                type="number"
+                placeholder="0"
+                className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+                value={price}
+                onChange={(e) => setprice(e.target.value)}
+                required
+              />
+            </div>
+            <div className="flex-1 flex flex-col gap-1 w-32">
+              <label className="text-base font-medium" htmlFor="offer-price">
+                Offer Price
+              </label>
+              <input
+                id="offer-price"
+                type="number"
+                placeholder="0"
+                className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+                value={offerprice}
+                onChange={(e) => setofferprice(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="px-8 py-2.5 bg-indigo-500 text-white font-medium rounded"
+          >
+            ADD
+          </button>
+        </form>
+      </div>
+      <div className="pr-40 pt-40 ">
+        <div className="w-60">
+          <div className="flex flex-col gap-2">
+            <div
+              className="w-full h-80  bg-cover bg-center rounded-xl bg-gray-400"
+              // get background image
+              style={{
+                backgroundImage: preview ? `url(${preview})` : "",
+              }}
+            ></div>
+            <div className="w-full h-20 flex flex-col gap-2">
+              <h1 className="font-semibold pl-1 pr-1 leading-tight">{name}</h1>
+              <h3 className="pl-1 pr-1 ">Rs {price}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
